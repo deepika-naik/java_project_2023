a982ea9b085cf0a0a451daf1846773e40553dccb
Deepa Naik <deepikanaik666@gmail.com>
2023-02-13 07:49:41 +0530

    Java  Basic
    Logical  operation

---
+++ src/day06/Program02.java
@@ -0,0 +1,38 @@
+package day06;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		
+				int a=10;
+				int b=20;
+				double c=20.0;
+				System.out.println("%%%%%%%%%%%%%%");
+				System.out.println(a>b);
+				System.out.println(b>a);
+				System.out.println(a<b);
+				System.out.println(b<a);
+				
+				System.out.println("%%%%%%%%%%%%%%");
+				System.out.println(a>=b);
+				System.out.println(b>=a);
+				System.out.println(a<=b);
+				System.out.println(b<=c);
+				
+				System.out.println("%%%%%%%%%%%%%%");
+				System.out.println(a==b);
+				System.out.println(b!=a);
+				System.out.println(b==c);
+				System.out.println(b!=c);
+				
+						
+				
+				
+
+			}
+
+	
+
+
+	}
+
---
+++ src/day06/Program03.java
@@ -0,0 +1,34 @@
+package day06;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		 int a=100;
+		 int b=20;
+		 int c=30;
+		 System.out.println((a>b)&&(a>c));
+		 System.out.println((b>a)&&(b>c));
+		 System.out.println((c>a)&&(c>b));
+		 
+		 System.out.println("############");
+		 System.out.println((a>b)||(a>c));
+		 System.out.println((b>a)||(b>c));
+		 System.out.println((c>a)||(c>b));
+		 System.out.println("%%%%%%%%%%");
+		 System.out.println(!(a>b));
+		 System.out.println(!(b>c));
+		 System.out.println(!(c>a));
+
+
+
+
+
+
+		 
+
+
+
+
+	}
+
+}
